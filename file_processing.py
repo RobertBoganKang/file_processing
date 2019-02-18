7ca2eb2842f50507e4c7e0f01e751e2b27113952
Robert Bogan Kang <bob.kang@qq.com>
2019-02-18 10:50:24 +0800

    Update file_processing.py

--- file_processing.py
+++ file_processing.py
@@ -1,9 +1,14 @@
 import glob
 import multiprocessing
 import os
+import shutil
 
 
 class FileProcessing(object):
+    """
+    recursively find file of processing
+    """
+
     def __init__(self, ops):
         # output folder
         self.output = os.path.abspath(ops.output)
@@ -18,13 +23,38 @@ class FileProcessing(object):
     # this section is default batch process  #
     # set above with self.input, self.output #
     ##########################################
+
+    def remove_empty_folder(self):
+        """
+        output folder may empty, then remove it
+        :return: None
+        """
+        # find all patterns
+        fs = glob.glob(os.path.join(self.output, '**/*'), recursive=True)
+        # select folders
+        fs = [x for x in fs if os.path.isdir(x)]
+        # test folder
+        for folder in fs:
+            if len(os.listdir(folder)) == 0:
+                shutil.rmtree(folder)
+
     def do_multiple(self):
+        """
+        parallel processing on files in file system
+        :return: None
+        """
         # find all patterns
         fs = glob.glob(os.path.join(self.input, '**/*.' + self.in_format), recursive=True)
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         pool.map(self.do_multiple_helper, fs)
+        self.remove_empty_folder()
 
     def do_multiple_helper(self, in_path):
+        """
+        prepare function for multiprocessing mapping
+        :param in_path: str; file path to process
+        :return: None
+        """
         # prepare output path
         truncated_path = os.path.split(in_path)[0][len(self.input) + 1:]
         out_folder = os.path.join(self.output, truncated_path)
@@ -35,6 +65,12 @@ class FileProcessing(object):
         self.do_single(in_path, out_folder)
 
     def do_single(self, in_path, out_folder):
+        """
+        single processing on
+        :param in_path: str; input file path
+        :param out_folder: str; output folder
+        :return: None
+        """
         out_name = os.path.split(in_path)[1][:-len(self.in_format)]
         out_path = os.path.join(out_folder, out_name) + self.out_format
         # the 'do_body' function is main function for batch process
