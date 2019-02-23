9f3ccbd1ed1f7770ca71f00692cb9bde5dcae033
Robert Bogan Kang <bob.kang@qq.com>
2019-02-23 21:47:17 +0800

    update

--- file_processing.py
+++ file_processing.py
@@ -10,16 +10,18 @@ class FileProcessing(object):
     """
 
     def __init__(self, ops):
-        # output folder
-        self.output = os.path.abspath(ops.output)
         # input folder
         self.input = os.path.abspath(ops.input)
+        # output folder
+        self.output = os.path.abspath(ops.output)
         # input format
         self.in_format = ops.in_format
         # output format
         self.out_format = ops.out_format
         # cpu number
         self.cpu = ops.cpu_number
+        # test mode: True: 1, False: 2 data flow
+        self.single_mode = self.output is None or self.out_format is None
 
     #########################################
     # this section is default batch process #
@@ -37,13 +39,14 @@ class FileProcessing(object):
             cpu_count = max_cpu
         return cpu_count
 
-    def remove_empty_folder(self):
+    @staticmethod
+    def remove_empty_folder(target_folder):
         """
-        output folder may empty, then remove it
+        target folder may empty, then remove it
         :return: None
         """
         # find all patterns
-        fs = glob.glob(os.path.join(self.output, '**/*'), recursive=True)
+        fs = glob.glob(os.path.join(target_folder, '**/*'), recursive=True)
         # select folders
         fs = [x for x in fs if os.path.isdir(x)]
         # test folder
@@ -51,8 +54,8 @@ class FileProcessing(object):
             if len(os.listdir(folder)) == 0:
                 shutil.rmtree(folder)
         # test if the output folder is empty
-        if len(os.listdir(self.output)) == 0:
-            shutil.rmtree(self.output)
+        if len(os.listdir(target_folder)) == 0:
+            shutil.rmtree(target_folder)
 
     def do_multiple(self):
         """
@@ -63,7 +66,10 @@ class FileProcessing(object):
         fs = glob.glob(os.path.join(self.input, '**/*.' + self.in_format), recursive=True)
         pool = multiprocessing.Pool(self.cpu_count())
         pool.map(self.do_multiple_helper, fs)
-        self.remove_empty_folder()
+        if self.single_mode:
+            self.remove_empty_folder(self.input)
+        else:
+            self.remove_empty_folder(self.output)
 
     def do_multiple_helper(self, in_path):
         """
@@ -71,32 +77,39 @@ class FileProcessing(object):
         :param in_path: str; file path to process
         :return: None
         """
-        # prepare output path
-        truncated_path = os.path.split(in_path)[0][len(self.input) + 1:]
-        out_folder = os.path.join(self.output, truncated_path)
-        # if not os.path.exists(out_folder):
-        #     print('[{}] has been created successfully ~'.format(out_folder))
-        os.makedirs(out_folder, exist_ok=True)
-        # do operation
-        self.do_single(in_path, out_folder)
+        if not self.single_mode:
+            # prepare output path
+            truncated_path = os.path.split(in_path)[0][len(self.input) + 1:]
+            out_folder = os.path.join(self.output, truncated_path)
+            # make directories
+            os.makedirs(out_folder, exist_ok=True)
+            # do operation
+            self.do_single(in_path, out_folder)
+        else:
+            self.do_single(in_path)
 
-    def do_single(self, in_path, out_folder):
+    def do_single(self, *args):
         """
         single processing on
-        :param in_path: str; input file path
-        :param out_folder: str; output folder
         :return: None
         """
-        out_name = os.path.split(in_path)[1][:-len(self.in_format)]
-        out_path = os.path.join(out_folder, out_name) + self.out_format
-        # the 'do_body' function is main function for batch process
-        self.do_body(in_path, out_path)
+        # in_path: str; input file path
+        # out_folder: str; output folder
+        if not self.single_mode:
+            in_path, out_folder = args[0], args[1]
+            out_name = os.path.split(in_path)[1][:-len(self.in_format)]
+            out_path = os.path.join(out_folder, out_name) + self.out_format
+            # the 'do_body' function is main function for batch process
+            self.do_body(in_path, out_path)
+        else:
+            in_path = args[0]
+            self.do_body(in_path)
 
     #######
     # end #
     #######
 
-    def do_body(self, in_path, out_path):
+    def do_body(self, *args):
         """
         do function will be implemented
         """
