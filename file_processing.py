8eaa09f4098dfd5a9d8ab347257732edd5948445
Robert Bogan Kang <bob.kang@qq.com>
2021-05-30 10:56:08 +0800

    add if read path list

--- file_processing.py
+++ file_processing.py
@@ -3,6 +3,7 @@ import glob
 import logging
 import multiprocessing as mp
 import os
+import re
 import shutil
 import signal
 import time
@@ -47,31 +48,24 @@ class FileProcessing(object):
     def __init__(self, ops):
         super().__init__()
         if isinstance(ops, dict):
-            # input folder
             self._input = os.path.abspath(ops['input'])
-            # output folder
             self._output = self._set_parser_value(ops, 'output', None, is_dict=True)
-            # input format
-            self._in_format = self._set_parser_value(ops, 'in_format', '??', is_dict=True)
-            # output format
+            self._in_format = self._set_parser_value(ops, 'in_format', '\\', is_dict=True)
             self._out_format = self._set_parser_value(ops, 'out_format', None, is_dict=True)
-            # cpu number
+            self._path_list = self._set_parser_value(ops, 'path_list', None, is_dict=True)
             self._cpu = self._set_parser_value(ops, 'cpu_number', 0, is_dict=True)
-            # logger level
             self._logger_level = self._set_parser_value(ops, 'logger_level', 'info', is_dict=True)
         else:
-            # input folder
             self._input = os.path.abspath(ops.input)
-            # output folder
             self._output = self._set_parser_value(ops, 'output', None)
-            # input format
-            self._in_format = self._set_parser_value(ops, 'in_format', '??')
-            # output format
+            self._in_format = self._set_parser_value(ops, 'in_format', '\\')
             self._out_format = self._set_parser_value(ops, 'out_format', None)
-            # cpu number
+            self._path_list = self._set_parser_value(ops, 'path_list', None)
             self._cpu = self._set_parser_value(ops, 'cpu_number', 0)
-            # logger level
             self._logger_level = self._set_parser_value(ops, 'logger_level', 'info')
+
+        # input controls
+        assert self._in_format is not None
 
         # fix output
         self._output = [os.path.abspath(self._output) if self._output is not None else None][0]
@@ -79,17 +73,12 @@ class FileProcessing(object):
         self._single_mode = self._output is None or self._out_format is None
         # pattern identifier
         self._pattern_identifier = '\\'
-        # is pattern
-        self._is_pattern = self._pattern_identifier in self._in_format
-        # in format is other pattern: `?` is no format, `??` is all format
-        self._is_no_format = '?' in self._in_format
-        self._is_all_format = self._in_format == '??'
-        # if out format pattern follow the same as input
-        self._is_same_out_format = self._in_format == '?'
+        # is pattern: regular expression
+        self._is_pattern = self._in_format.startswith(self._pattern_identifier)
         # empty folder counter
         self._empty_file_counter = 0
         self._total_file_number = None
-        self._stop_cleaning_ratio = 0.1
+        self._stop_each_file_cleaning_ratio = 0.1
         # logger
         self.logger = None
         self._logger_folder = 'log'
@@ -115,6 +104,7 @@ class FileProcessing(object):
 
     @staticmethod
     def _set_parser_value(ops, parser_name, default_value, is_dict=False):
+        """ set parser value to default if needed """
         value = None
         if parser_name in ops:
             if is_dict:
@@ -154,10 +144,7 @@ class FileProcessing(object):
 
     @staticmethod
     def _remove_empty_folder(target_folder):
-        """
-        target folder may empty, then remove it
-        :return: None
-        """
+        """ target folder may empty, then remove it """
         for root, dirs, files in os.walk(target_folder, topdown=False):
             for name in dirs:
                 dir_path = os.path.join(root, name)
@@ -186,11 +173,7 @@ class FileProcessing(object):
                     shutil.rmtree(folder)
 
     def _do_multiple_helper(self, in_path):
-        """
-        prepare function for multiprocessing mapping
-        :param in_path: str; file path to process
-        :return: None
-        """
+        """ prepare function for multiprocessing mapping """
         if not self._single_mode:
             # prepare output path
             truncated_path = os.path.split(in_path)[0][len(self._input) + 1:]
@@ -204,56 +187,79 @@ class FileProcessing(object):
             self._do_single(in_path)
 
     def _do_single(self, *args):
-        """
-        single processing on
-        :return: None
-        """
+        """ single process """
         # in_path: str; input file path
         # out_folder: str; output folder
         if not self._single_mode:
             in_path, out_folder = args[0], args[1]
             out_name = os.path.split(in_path)[1]
-            if self._is_pattern or self._is_same_out_format:
+            if self._is_pattern:
                 out_path = os.path.join(out_folder, out_name)
             else:
                 # if not pattern, truncated the format and add a new one
-                if len(self._in_format) > 0:
-                    out_name = out_name[:-len(self._in_format)]
-                else:
-                    out_name += '.'
+                out_name = out_name[:-len(self._in_format)]
                 out_path = os.path.join(out_folder, out_name) + self._out_format
             # the 'do' function is main function for batch process
             self.do(in_path, out_path)
             return out_path
+        # only use `in_path`
         else:
             in_path = args[0]
             self.do(in_path)
 
+    def _find_fs(self):
+        """ find files from glob function """
+        # find all patterns from regular expression
+        if self._is_pattern:
+            # if contains `pattern_identifier`, it is considered to be regular expression restrictions
+            pattern = self._in_format[len(self._pattern_identifier):]
+            fs = glob.glob(os.path.join(self._input, '**/*'), recursive=True)
+            fs = [x for x in fs if re.search(pattern, os.path.split(x)[-1]) is not None]
+        else:
+            fs = glob.glob(os.path.join(self._input, '**/*.' + self._in_format), recursive=True)
+        return fs
+
+    @staticmethod
+    def _get_common_path(path_a, path_b):
+        """ get common path from two given paths """
+        i = 0
+        while i < min(len(path_a), len(path_b)):
+            if path_a[i] == path_b[i]:
+                i += 1
+            else:
+                break
+        return path_a[:i]
+
+    def _read_fs(self):
+        """ read paths from text file: one line with one path """
+        if not os.path.exists(self._path_list):
+            raise FileNotFoundError(f'WARNING: [{self._path_list}] not found!')
+        fs = []
+        with open(self._path_list, 'r') as f:
+            lines = f.readlines()
+            common_path = lines[0]
+            for line in lines:
+                path = os.path.abspath(line.strip())
+                if self._is_pattern:
+                    pattern = self._in_format[len(self._pattern_identifier):]
+                    condition = re.search(pattern, os.path.split(path)[-1]) is not None
+                else:
+                    condition = path.endswith('.' + self._in_format)
+                if os.path.exists(path) and condition:
+                    fs.append(path)
+                    common_path = self._get_common_path(path, common_path)
+        return common_path, fs
+
     def do(self, *args):
-        """
-        do function will be implemented on files, please rewrite this method
-        """
+        """ do function will be implemented on files, please rewrite this method """
         pass
 
     def __call__(self):
-        """
-        parallel processing on files in file system
-        :return: None
-        """
-        # find all patterns
-        if self._is_pattern:
-            # if contains `pattern_identifier`, it is considered to be patterns
-            fs = glob.glob(os.path.join(self._input, '**/' + self._in_format.replace(self._pattern_identifier, '')),
-                           recursive=True)
+        """ parallel processing on files in file system """
+        if self._path_list is None:
+            fs = self._find_fs()
         else:
-            if self._is_no_format:
-                fs = glob.glob(os.path.join(self._input, '**/*' + self._in_format), recursive=True)
-                if not self._is_all_format:
-                    fs = [x for x in fs if '.' not in x]
-                # reset input format to empty
-                self._in_format = ''
-            else:
-                fs = glob.glob(os.path.join(self._input, '**/*.' + self._in_format), recursive=True)
+            fs = self._read_fs()
         fs = [x for x in fs if os.path.isfile(x)]
         self._total_file_number = len(fs)
         if not self._total_file_number:
@@ -268,9 +274,9 @@ class FileProcessing(object):
                     if not os.path.exists(file_path):
                         self._empty_file_counter += 1
                     if not self._single_mode and (
-                            self._empty_file_counter / self._total_file_number < self._stop_cleaning_ratio):
+                            self._empty_file_counter / self._total_file_number < self._stop_each_file_cleaning_ratio):
                         self._simplify_path(self._output, file_path)
-                    # update pbar
+                    # update p_bar
                     p_bar.update()
 
                 for f in fs:
@@ -283,7 +289,8 @@ class FileProcessing(object):
                 self._do_multiple_helper(f)
 
         # clean output folder
-        if not self._single_mode and (self._empty_file_counter / self._total_file_number >= self._stop_cleaning_ratio):
+        if not self._single_mode and (
+                self._empty_file_counter / self._total_file_number >= self._stop_each_file_cleaning_ratio):
             self._remove_empty_folder(self._output)
         # remove empty log
         self._remove_empty_file(self._log_path)
--- template.py
+++ template.py
@@ -36,16 +36,20 @@ class Template(FileProcessing):
 
 
 if __name__ == '__main__':
-    # prepare argument
     parser = argparse.ArgumentParser(description='xxx xxx xxx')
-    parser.add_argument('--input', '-i', help='xxx', default='in', type=str)
-    parser.add_argument('--output', '-o', help='xxx', default=None, type=str)
-    parser.add_argument('--cpu_number', '-j', type=int, help='cpu number of processing', default=0)
-    parser.add_argument('--in_format', '-if', type=str, help='define the input format', default='xxx')
-    parser.add_argument('--out_format', '-of', type=str, help='define the output format', default=None)
-    parser.add_argument('--logger_level', '-log', type=str,
-                        help='define the logger level, if `None`: no log file generated', default=None)
+    fp_group = parser.add_argument_group('file processing arguments')
+    fp_group.add_argument('--input', '-i', help='the input folder', default='in', type=str)
+    fp_group.add_argument('--output', '-o', help='the output folder', default=None, type=str)
+    fp_group.add_argument('--in_format', '-if', type=str, help='the input format', default='xxx')
+    fp_group.add_argument('--out_format', '-of', type=str, help='the output format', default=None)
+    fp_group.add_argument('--path_list', '-l', type=str, help='the file for paths', default=None)
+    fp_group.add_argument('--cpu_number', '-j', type=int, help='cpu number of processing', default=0)
+    fp_group.add_argument('--logger_level', '-log', type=str,
+                          help='define the logger level, if `None`: no log file generated', default=None)
+
+    xx_group = parser.add_argument_group('xxx xxx xxx')
     # ...
+
     args = parser.parse_args()
 
     # or use dict
