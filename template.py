524b25a1200c35169dcbd6c5cec05b36db338c8b
Robert Bogan Kang <bob.kang@qq.com>
2021-06-17 19:50:08 +0800

    fix template

--- template.py
+++ template.py
@@ -11,56 +11,47 @@ class Template(FileProcessing):
 
     def __init__(self, ops):
         super().__init__(ops)
-        self.xx = None
-        # ...
 
     # in_path --> out_path; data flow
     def do(self, in_path, out_path):
         """
-        overwrite the method
+        implement this method
         """
         pass
 
     # target path operation
     # def do(self, target_path):
     #    """
-    #    overwrite the method
+    #    implement this method
     #    """
     #    pass
-
-    def xxx(self):
-        # xxx
-        pass
-
-    # ...
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='xxx xxx xxx')
     fp_group = parser.add_argument_group('file processing arguments')
-    fp_group.add_argument('--input', '-i', help='the input folder/file, or a text file for paths', type=str,
+    fp_group.add_argument('--input', '-i', type=str, help='the input folder/file, or a text file for paths',
                           default='in')
     fp_group.add_argument('--in_format', '-if', type=str, help='the input format', default='xxx')
-    fp_group.add_argument('--output', '-o', help='the output folder/file', type=str, default='out')
+    fp_group.add_argument('--output', '-o', type=str, help='the output folder/file', default='out')
     fp_group.add_argument('--out_format', '-of', type=str, help='the output format', default='yyy')
     fp_group.add_argument('--cpu_number', '-j', type=int, help='cpu number of processing', default=0)
     fp_group.add_argument('--logger_level', '-log', type=str,
                           help='define the logger level, if `None`: no log file generated', default=None)
 
-    xx_group = parser.add_argument_group('xxx xxx xxx')
-    # ...
+    xx_group = parser.add_argument_group('xxx xxx arguments')
 
     args = parser.parse_args()
 
     # or use dict
     # args = {
     #     'input': 'in',
-    #     'output': 'out',
-    #     'cpu_number': 0,
     #     'in_format': 'xxx',
-    #     'out_format': None
+    #     'output': 'out',
+    #     'out_format': 'yyy',
+    #     'cpu_number': 0,
+    #     'logger_level': None
     # }
 
     # do operation
     Template(args)()
-    # ...
