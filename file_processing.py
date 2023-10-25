7476517ab010cca9b44d840279987e9a8c6a2f3a
RobertBoganKang\robert <bob.kang@qq.com>
2023-10-25 10:29:46 +0800

    remove abspath in read txt file mode

--- file_processing.py
+++ file_processing.py
@@ -367,10 +367,7 @@ class FileProcessing(object):
             return None, fs
         common_path = lines[0]
         for line in lines:
-            if self.fp_in_format != '!':
-                path = os.path.abspath(line.strip())
-            else:
-                path = line
+            line = line.strip()
             if self._is_skip_pattern or self._check_input_file_path(path):
                 fs.append(path)
                 common_path = self._get_common_path(path, common_path)
